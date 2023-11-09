ee1b10a1c5c45ca1b34bbe45598f2b5b3b3bc598
Juliana Marques <marquesdjuliana@gmail.com>
2023-11-09 14:29:09 -0300

    Create tests for ingredients.py

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,29 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501!
+from src.models.ingredient import (
+    Ingredient,
+    Restriction,
+)  # noqa: F401, E261, E501!
 
 
 # Req 1
 def test_ingredient():
-    pass
+    queijo_mussarela = Ingredient("queijo mussarela")
+    queijo_prov = Ingredient("queijo provolone")
+
+    assert queijo_mussarela.name == "queijo mussarela"
+    assert queijo_mussarela.restrictions == {
+        Restriction.LACTOSE,
+        Restriction.ANIMAL_DERIVED,
+    }
+    assert hash(queijo_mussarela) == hash(queijo_mussarela.name)
+    assert queijo_mussarela == Ingredient("queijo mussarela")
+    assert queijo_mussarela != queijo_prov
+    assert repr(queijo_mussarela) == "Ingredient('queijo mussarela')"
+
+    assert queijo_prov.name == "queijo provolone"
+    assert queijo_prov.restrictions == {
+        Restriction.LACTOSE,
+        Restriction.ANIMAL_DERIVED,
+    }
+    assert hash(queijo_prov) == hash(queijo_prov.name)
+    assert queijo_prov == Ingredient("queijo provolone")
+    assert repr(queijo_prov) == "Ingredient('queijo provolone')"
